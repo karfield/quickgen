58e08740302f069f91223ea82e51478c2e6e354e
Karfield Chen <kf@karfield.com>
2017-03-27 15:36:15 +0800

    add java-cli-assembly and java-cli-shade
    Signed-off-by: Karfield Chen <kf@karfield.com>

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 | name | description |
 |---|---|
 | java-webapp | generate a web java application based on a maven archetype |
+| edas-echo-demo | generate a Aliyun EDAS example codes |
---
+++ java-cli-assembly/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by karfield on 27/03/2017.
+ */
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("Hello CLI User!");
+    }
+}
